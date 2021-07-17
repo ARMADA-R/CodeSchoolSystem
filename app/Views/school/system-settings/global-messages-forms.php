f05c65a4c20e3c4ac2de180254ccd6e5360fc4c9
ARMADA <rafy199889@gmail.com>
2021-07-17 20:55:32 +0300

    some editing :)

--- app/Views/school/system-settings/global-messages-forms.php
+++ app/Views/school/system-settings/global-messages-forms.php
@@ -1,17 +1,47 @@
-<?php require (APPPATH.'views/school/layouts/preContent.php') ?>
+<?php require(APPPATH . 'views/school/layouts/preContent.php') ?>
 
+<!-- Content Header (Page header) -->
+<div class="content-header my-2 bg-white">
 
-<div class="row">
-    <div class="col-12">
+    <div class="row ">
+        <div class="col ">
+            قوالب الرسائل العامة
+        </div>
+    </div>
+</div>
+<!-- /.content-header -->
+
+<div class="row mt-4">
+    <div class="col-md-4">
         <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Home</h3>
-            </div>
-            <!-- /.card-header -->
             <div class="card-body text-center">
-                <h1>
-                    Home Page
-                </h1>
+                <h3>
+                    Content
+                </h3>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body text-center">
+                <h3>
+                    Content
+                </h3>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body text-center">
+                <h3>
+                    Content
+                </h3>
             </div>
             <!-- /.card-body -->
         </div>
@@ -22,5 +52,15 @@
 </div>
 <!-- /.row -->
 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+</div>
 
-<?php include_once(APPPATH.'views/school/layouts/postContent.php') ?>
+
+<?php include_once(APPPATH . 'views/school/layouts/postContent.php') ?>
